ff4bbca55ddebca47632c1d875e8169485d9b79e
Hassan Riaz <hassanriaz2625@gmail.com>
2023-05-27 17:21:46 +0500

    S6,S7 save recode  from  console and save recode in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 1st 
+# student = Student.new(:name =>"hassan", :age => "23",:father_name => "Riaz",:phone_number => "090078601",:email => "hassan@2625",
+# :blood_group => "B-" )
+
+# student.save
+
+# 2nd 
+# Student.create(:name =>"Ali", :age => "48",:father_name => "Hamza",:phone_number => "0080",:email => "aliahmed@625",:blood_group =
+# > "B-" )
+
+# 3rd 
+# student = Student.create(:name =>"ABR", :age => "28",:father_name => "Riaz",:phone_number => "233480",:email => "abr@625",:blood_
+# group => "A-" )
