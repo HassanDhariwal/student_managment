134d25e800f882d142b21fe1fd7a0fe9e5e2c99e
Hassan Riaz <hassanriaz2625@gmail.com>
2023-05-30 09:11:27 +0500

    add index show new template and action make form with

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,4 +1,34 @@
 class StudentController < ApplicationController
+  
   def index
+    @students = Student.all
   end
+
+  def show
+    @student = Student.find(params[:id])
+
+  end
+
+  def new
+    @student = Student.new({:name => 'Enter your name', :father_name => 'Enter your Father Name '})
+  end   
+
+  def create
+    @student = Student.new( 
+    name: params[:student][:name],
+    age: params[:student][:age], 
+    father_name: params[:student][:father_name], 
+    Phone_number: params[:student][:Phone_number],
+    Email: params[:student][:Email],
+    Blood_group: params[:student][:Blood_group],
+    Created: params[:student][:Created],
+    Updated: params[:student][:Updated]
+    )
+    if @student.save
+      redirect_to student_index_path
+    else
+      render(new)
+    end    
+  end
+
 end
