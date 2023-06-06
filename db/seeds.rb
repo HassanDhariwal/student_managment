02411a581b6c8a38a58630ca3eb8d61580fa20e7
Hassan Riaz <hassanriaz2625@gmail.com>
2023-06-06 17:23:13 +0500

    S13 & S14 create a new table Class_Student and recode enter with rails c

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,28 @@
 # 3rd 
 # student = Student.create(:name =>"ABR", :age => "28",:father_name => "Riaz",:phone_number => "233480",:email => "abr@625",:blood_
 # group => "A-" )
+
+# StudentClass Table 2
+# First recode  
+# s_class = ap StudentClass.create(section_name: "B", teach  er_name: "Ahmed", student_name: "Ali" )
+# 2nd recode
+# s_class = StudentClass.new(section_name: "A", teacher_name: "Ali", student_name: "abubakr" )
+# 3rd recode
+# s_class = StudentClass.create(section_name: "C", teacher_name: "ZAiN", student_name: "ABR" )
+# 4th recode
+# s_class = StudentClass.create(section_name: "A", teacher_name: "farooq", student_name: "ali" )
+# 5th recode
+# s_class = StudentClass.new(section_name: "A", teacher_name: "farooq_ali", student_name: "ali" )
+# 2.7.1 :043 > s_class.save
+# 5th recode not Write this 
+# s_class = ap StudentClass.new(section_name: "A", teacher_
+# #<StudentClass:0x00005589c1319408> {
+#               :id => nil,
+#     :section_name => "A",
+#     :teacher_name => "Ali",
+#       :updated_at => nil
+# }
+# 2.7.1 :011 > s_class.save
+# Traceback (most recent call last):
+#         1: from (irb):11
+# NoMethodError (undefined method `save' for nil:NilClass)
