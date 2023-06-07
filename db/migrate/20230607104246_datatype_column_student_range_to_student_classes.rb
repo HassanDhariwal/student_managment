fdf03186ae3d440c80198fe04b82ff10c1f1e9c8
Hassan Riaz <hassanriaz2625@gmail.com>
2023-06-07 17:00:27 +0500

    S18 rename column type/studentclass create recode by console

---
+++ db/migrate/20230607104246_datatype_column_student_range_to_student_classes.rb
@@ -0,0 +1,7 @@
+class DatatypeColumnStudentRangeToStudentClasses < ActiveRecord::Migration[5.1]
+  def change        
+    
+    change_column("student_classes", "student_range", :integer)
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20230607074056) do
+ActiveRecord::Schema.define(version: 20230607104246) do
 
   create_table "student_classes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3" do |t|
     t.string "section_name"
     t.string "teacher_name"
-    t.string "student_range"
+    t.integer "student_range"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "grade"
